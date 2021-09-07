419c340e2ba8ad13f8b581877458242b0cd45d1c
Asim <asim.awan@outlook.com>
2021-09-07 21:32:05 +0000

    Refactored routes for error handling

--- express_server.js
+++ express_server.js
@@ -9,13 +9,14 @@ app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
-const generateRandomString = function (len) {
-  return Math.random().toString(20).substr(2, len);
-};
-
-const checkUrl = (url) => {
-  return urlDatabase[url];
-};
+const {
+  generateRandomString,
+  checkUrl,
+  verifyUser,
+  getUserInfo,
+  createUser,
+  verifyUserEmail
+} = require("./helper_functions");
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -39,23 +40,28 @@ app.get("/", (req, res) => {
   res.send("Hello");
 });
 
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase, user: users[req.cookies.user_id] };
   res.render("urls_index", templateVars);
 });
 
+
 app.get("/urls/new", (req, res) => {
   const templateVars = { user: users[req.cookies.user_id] };
   res.render("urls_new", templateVars);
 });
+
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
@@ -66,10 +72,12 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
+
 
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString(6);
@@ -77,11 +85,13 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
+
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL;
   delete urlDatabase[shortURL];
   res.redirect("/urls");
 });
+
 
 app.post("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
@@ -89,59 +99,56 @@ app.post("/urls/:shortURL", (req, res) => {
   res.redirect("/urls");
 });
 
+
 app.post("/login", (req, res) => {
   const user_email = req.body.email;
-  const user_pass = req.body.password
-  console.log(user_email);
-  console.log(user_pass);
-  for (let user in users) {
-    if (users[user].email === user_email && users[user].password === user_pass) {
-      const user_id = users[user].id
-      res.cookie("user_id", user_id);
-      // templateVars = { user: users[req.cookies.user_id] };
-      res.redirect("/urls");
+  const user_pass = req.body.password;
+
+  if (getUserInfo (user_email, users)) {
+    const pass = getUserInfo(user_email, users).password;
+    const id = getUserInfo(user_email, users).id;
+    if (pass !== user_pass) {
+      res.status(403).send('Error 403... Incorrect password')
+    } else {
+      res.cookie('user_id', id);
+      res.redirect('/urls');
     }
+  } else {
+    res.status(403).send('Error 403... Email not found')
   }
-  res.redirect("/register");
 });
+
 
 app.get("/login", (req, res) => {
   templateVars = { user: users[req.cookies.user_id] };
   res.render("urls_login", templateVars);
 });
 
+
 app.post("/logout", (req, res) => {
   res.clearCookie("user_id");
   res.redirect("/urls");
 });
+
 
 app.get("/register", (req, res) => {
   templateVars = { user: users[req.cookies.user_id] };
   res.render("urls_register", templateVars);
 });
 
-app.post("/register", (req, res) => {
-  for (let user in users) {
-    if (users[user].email === req.body.email) {
-      res.status(400).send("This email is already registered");
-    }
-  }
 
-  if (req.body.email === '' || req.body.password === '') {
+app.post("/register", (req, res) => {
+  if (req.body.email === "" || req.body.password === "") {
     res.status(400).send("Email or Password not entered");
   }
-
-  const user_id = generateRandomString(4);
-  const obj = {
-    id: user_id,
-    email: req.body.email,
-    password: req.body.password,
-  };
-  users[user_id] = obj;
-
-  res.cookie("user_id", user_id);
+  if (verifyUserEmail(req.body, users)) {
+    res.status(400).send("This email is already registered");
+  }
+  const newUser = createUser(req.body, users);
+  res.cookie("user_id", newUser.id);
   res.redirect("/urls");
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
